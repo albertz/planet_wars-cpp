4c632c7c9611f202a43db152b50df3d158fd9e62
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-09-12 02:48:07 +0200

    utils

--- utils.cpp
+++ utils.cpp
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-long getMilliseconds() {
+long currentTimeMillis() {
 	struct Timeval : timeval {
 		Timeval() {
 			gettimeofday(this, NULL);
@@ -23,4 +23,31 @@ long getMilliseconds() {
 
 	mtime = ((seconds) * 1000 + useconds/1000.0) + 0.5;
 	return mtime;
+}
+
+typedef unsigned char uchar;
+
+std::string TrimSpaces(const std::string& str) {	
+	size_t start = 0;
+	for(size_t i = 0; i < str.size(); ++i)
+		if(!isspace((uchar)str[i]) || isgraph((uchar)str[i])) {
+			start = i;
+			break;
+		}
+	
+	size_t n = 0;
+	for(size_t i = str.size(); i > start; --i)
+		if(!isspace((uchar)str[i-1]) || isgraph((uchar)str[i-1])) {
+			n = i - start - 1;
+			break;
+		}
+	
+	return str.substr(start, n);
+}
+
+std::string ToLower(const std::string& str) {
+	std::string res;
+	for(std::string::const_iterator i = str.begin(); i != str.end(); i++)
+		res += tolower((uchar)*i);
+	return res;
 }
--- utils.h
+++ utils.h
@@ -1,3 +1,6 @@
+// code under GPLv3
+// by Albert Zeyer
+
 #ifndef __PW__UTILS_H__
 #define __PW__UTILS_H__
 
@@ -11,6 +14,16 @@ std::string to_string(T val) {
 	return oss.str();
 }
 
-long getMilliseconds();
+long currentTimeMillis();
+
+template<typename T>
+bool AllTrue(const T& container) {
+	for(typename T::const_iterator i = container.begin(); i != container.end(); ++i)
+		if(!*i) return false;
+	return true;
+}
+
+std::string TrimSpaces(const std::string& str);
+std::string	ToLower(const std::string& str);
 
 #endif
