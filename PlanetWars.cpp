43eb7f73e21e2d25d7bd8fe3b9bb5a96030b3fee
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-09-12 15:44:50 +0200

    reimplement planet

--- PlanetWars.cpp
+++ PlanetWars.cpp
@@ -63,60 +63,6 @@ int Fleet::TurnsRemaining() const {
   return turns_remaining_;
 }
 
-Planet::Planet(int planet_id,
-               int owner,
-               int num_ships,
-               int growth_rate,
-               double x,
-               double y) {
-  planet_id_ = planet_id;
-  owner_ = owner;
-  num_ships_ = num_ships;
-  growth_rate_ = growth_rate;
-  x_ = x;
-  y_ = y;
-}
-
-int Planet::PlanetID() const {
-  return planet_id_;
-}
-
-int Planet::Owner() const {
-  return owner_;
-}
-
-int Planet::NumShips() const {
-  return num_ships_;
-}
-
-int Planet::GrowthRate() const {
-  return growth_rate_;
-}
-
-double Planet::X() const {
-  return x_;
-}
-
-double Planet::Y() const {
-  return y_;
-}
-
-void Planet::Owner(int new_owner) {
-  owner_ = new_owner;
-}
-
-void Planet::NumShips(int new_num_ships) {
-  num_ships_ = new_num_ships;
-}
-
-void Planet::AddShips(int amount) {
-  num_ships_ += amount;
-}
-
-void Planet::RemoveShips(int amount) {
-  num_ships_ -= amount;
-}
-
 PlanetWars::PlanetWars(const std::string& gameState) {
   ParseGameState(gameState);
 }
--- PlanetWars.h
+++ PlanetWars.h
@@ -70,52 +70,25 @@ class Fleet {
 
 // Stores information about one planet. There is one instance of this class
 // for each planet on the map.
-class Planet {
- public:
-  // Initializes a planet.
-  Planet(int planet_id,
-         int owner,
-         int num_ships,
-         int growth_rate,
-         double x,
-         double y);
+struct Planet {
+	int planetId;
+	int owner;
+	int numShips;
+	int growthRate;
+	double x, y;
 
-  // Returns the ID of this planets. Planets are numbered starting at zero.
-  int PlanetID() const;
-
-  // Returns the ID of the player that owns this planet. Your playerID is
-  // always 1. If the owner is 1, this is your planet. If the owner is 0, then
-  // the planet is neutral. If the owner is 2 or some other number, then this
-  // planet belongs to the enemy.
-  int Owner() const;
-
-  // The number of ships on the planet. This is the "population" of the planet.
-  int NumShips() const;
-
-  // Returns the growth rate of the planet. Unless the planet is neutral, the
-  // population of the planet grows by this amount each turn. The higher this
-  // number is, the faster this planet produces ships.
-  int GrowthRate() const;
-
-  // The position of the planet in space.
-  double X() const;
-  double Y() const;
-
-  // Use the following functions to set the properties of this planet. Note
-  // that these functions only affect your program's copy of the game state.
-  // You can't steal your opponent's planets just by changing the owner to 1
-  // using the Owner(int) function! :-)
-  void Owner(int new_owner);
-  void NumShips(int new_num_ships);
-  void AddShips(int amount);
-  void RemoveShips(int amount);
-
- private:
-  int planet_id_;
-  int owner_;
-  int num_ships_;
-  int growth_rate_;
-  double x_, y_;
+	// Initializes a planet.
+	Planet(int _planet_id,
+		   int _owner,
+		   int _num_ships,
+		   int _growth_rate,
+		   double _x,
+		   double _y)
+	: planetId(_planet_id), owner(_owner), numShips(_num_ships),
+	growthRate(_growth_rate), x(_x), y(_y) {}
+	
+	void AddShips(int amount) { numShips += amount; }
+	void RemoveShips(int amount) { numShips -= amount; }
 };
 
 class PlanetWars {
