005c6a3309c0dc4dd236035ee43c511277a42e84
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-09-13 17:00:06 +0200

    viewer

---
+++ viewer.cpp
@@ -0,0 +1,19 @@
+/*
+ *  viewer.cpp
+ *  PlanetWars
+ *
+ *  Created by Albert Zeyer on 13.09.10.
+ *  code under GPLv3
+ *
+ */
+
+#include <SDL.h>
+#include "viewer.h"
+#include "gfx.h"
+#include "game.h"
+
+void DrawGame(const Game& game, SDL_Surface* surf) {
+	FillSurface(surf, Color(0, 0, 0));
+
+	
+}
---
+++ viewer.h
@@ -0,0 +1,44 @@
+/*
+ *  viewer.h
+ *  PlanetWars
+ *
+ *  Created by Albert Zeyer on 13.09.10.
+ *  coder under GPLv3
+ *
+ */
+
+#ifndef __PW__VIEWER_H__
+#define __PW__VIEWER_H__
+
+#include <iterator>
+#include <list>
+#include "game.h"
+
+struct SDL_Surface;
+
+void DrawGame(const Game& game, SDL_Surface* surf);
+
+struct Viewer {
+	std::list<Game> gameStates;
+	std::list<Game>::iterator currentState;
+	
+	void init() { currentState = gameStates.begin(); }
+	bool ready() { return !gameStates.empty(); }
+	bool next() {
+		if(!ready()) return false;
+		std::list<Game>::iterator n = currentState; ++n;
+		if(n == gameStates.end()) return false;
+		++currentState; return true;
+	}
+	bool last() {
+		if(!ready()) return false;
+		if(currentState == gameStates.begin()) return false;
+		--currentState; return true;
+	}
+	void draw(SDL_Surface* surf = SDL_GetVideoSurface()) {
+		if(ready())
+			DrawGame(*currentState, surf);
+	}
+};
+
+#endif
