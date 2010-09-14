72c7c6108949d8dce555224b3b666c7750d4bb06
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-09-15 01:06:30 +0200

    extensions for fpn

--- FixedPointNumber.h
+++ FixedPointNumber.h
@@ -21,18 +21,33 @@ struct FixedPointNumber {
 	FixedPointNumber(int n) : number(n * factor) {}
 	FixedPointNumber(Sint64 n) : number(n * factor) {}
 	FixedPointNumber(double n) : number(Sint64(n * factor)) {}
+	static FixedPointNumber raw(Sint64 num) { FixedPointNumber n; n.number = num; return n; }
+	static FixedPointNumber minGreater0() { return raw(1); }
 	
 	FixedPointNumber& operator++/* prefix */(int) { number += factor; return *this; }
 	FixedPointNumber& operator--/* prefix */(int) { number -= factor; return *this; }
+	FixedPointNumber& operator+=(const FixedPointNumber& n) { number += n.number; return *this; }
 	FixedPointNumber& operator-=(const FixedPointNumber& n) { number -= n.number; return *this; }
+
+	FixedPointNumber operator-() const { return raw(-number); }	
+	FixedPointNumber operator+(const FixedPointNumber& n) const { return raw(number + n.number); }
+	FixedPointNumber operator-(const FixedPointNumber& n) const { return raw(number - n.number); }
+	FixedPointNumber operator*(const FixedPointNumber& n) const { return raw(number * n.number / Sint64(factor)); }
+	double operator*(double n) const { return number * (n / factor); }
+	FixedPointNumber operator/(const FixedPointNumber& n) const { return raw(number * factor / n.number); }
+	FixedPointNumber operator%(const FixedPointNumber& n) const { return raw(number % n.number); }
 	
 	bool operator==(const FixedPointNumber& n) const { return number == n.number; }
 	bool operator!=(const FixedPointNumber& n) const { return !(*this == n); }
 	bool operator<(const FixedPointNumber& n) const { return number < n.number; }
 	bool operator>(const FixedPointNumber& n) const { return number > n.number; }
+	bool operator<=(const FixedPointNumber& n) const { return number <= n.number; }
+	bool operator>=(const FixedPointNumber& n) const { return number >= n.number; }
 	
-	int asInt() { return (int)(number / factor); }
-	
+	int asInt() const { return (int)(number / Sint64(factor)); }
+	double asDouble() const { return double(number) / factor; }
+	FixedPointNumber abs() const { return raw((number >= 0) ? number : -number); }
+	FixedPointNumber floor() const { return raw((number >= 0) ? (number - number % factor) : (number - factor + 1 + (-number - 1) % factor)); }
 };
 
 #endif
