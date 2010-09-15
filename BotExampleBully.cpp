9231ca79b6572cb63f2c9888b0ff8139ae33499a
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-09-15 23:51:00 +0200

    ported bully

--- BotExampleBully.cpp
+++ BotExampleBully.cpp
@@ -1,65 +1,59 @@
-import java.util.*;
+#include <iostream>
+#include "game.h"
 
-public class BullyBot {
-    public static void DoTurn(PlanetWars pw) {
+static void DoTurn(Game& pw) {
 	// (1) If we current have a fleet in flight, just do nothing.
-	if (pw.MyFleets().size() >= 1) {
+	if (pw.state.fleets.size() >= 1) {
 	    return;
 	}
 	// (2) Find my strongest planet.
-	Planet source = null;
-	double sourceScore = Double.MIN_VALUE;
-	for (Planet p : pw.MyPlanets()) {
-	    double score = (double)p.NumShips();
+	int source = -1;
+	double sourceScore = -1;
+	for (size_t p = 0; p < pw.NumPlanets(); ++p) {
+		if(pw.state.planets[p].owner != 1) continue;
+	    double score = (double)pw.state.planets[p].numShips;
 	    if (score > sourceScore) {
-		sourceScore = score;
-		source = p;
+			sourceScore = score;
+			source = p;
 	    }
 	}
 	// (3) Find the weakest enemy or neutral planet.
-	Planet dest = null;
-	double destScore = Double.MIN_VALUE;
-	for (Planet p : pw.NotMyPlanets()) {
-	    double score = 1.0 / (1 + p.NumShips());
+	int dest = -1;
+	double destScore = -1;
+	for (size_t p = 0; p < pw.NumPlanets(); ++p) {
+		if(pw.state.planets[p].owner == 1) continue;
+	    double score = 1.0 / (1 + pw.state.planets[p].numShips);
 	    if (score > destScore) {
-		destScore = score;
-		dest = p;
+			destScore = score;
+			dest = p;
 	    }
 	}
 	// (4) Send half the ships from my strongest planet to the weakest
 	// planet that I do not own.
-	if (source != null && dest != null) {
-	    int numShips = source.NumShips() / 2;
+	if (source >= 0 && dest >= 0) {
+	    int numShips = pw.state.planets[source].numShips / 2;
 	    pw.IssueOrder(source, dest, numShips);
 	}
-    }
-
-    public static void main(String[] args) {
-	String line = "";
-	String message = "";
-	int c;
-	try {
-	    while ((c = System.in.read()) >= 0) {
-		switch (c) {
-		case '\n':
-		    if (line.equals("go")) {
-			PlanetWars pw = new PlanetWars(message);
-			DoTurn(pw);
-		        pw.FinishTurn();
-			message = "";
-		    } else {
-			message += line + "\n";
-		    }
-		    line = "";
-		    break;
-		default:
-		    line += (char)c;
-		    break;
-		}
-	    }
-	} catch (Exception e) {
-	    // Owned.
-	}
-    }
 }
 
+int main(int argc, char *argv[]) {
+	std::string current_line;
+	std::string map_data;
+	while (true) {
+		int c = std::cin.get();
+		current_line += (char)c;
+		if (c == '\n') {
+			if (current_line.length() >= 2 && current_line.substr(0, 2) == "go") {
+				Game game;
+				game.ParseGameState(map_data);
+				map_data = "";
+				DoTurn(game);
+				game.FinishTurn();
+			} else {
+				map_data += current_line;
+			}
+			current_line = "";
+		}
+	}
+	return 0;
+}
