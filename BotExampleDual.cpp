586433172cbdd6f0cc90f7c9461654f36afcefc3
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-09-16 00:25:11 +0200

    ported dual

--- BotExampleDual.cpp
+++ BotExampleDual.cpp
@@ -1,21 +1,21 @@
-import java.util.*;
+#include <iostream>
+#include "game.h"
 
-public class DualBot {
-    public static void DoTurn(PlanetWars pw) {
-        int numFleets = 1;
-	boolean attackMode = false;
-	if (pw.NumShips(1) > pw.NumShips(2)) {
+static void DoTurn(const Game& pw) {
+	unsigned int numFleets = 1;
+	bool attackMode = false;
+	if (pw.state.NumShips(1) > pw.state.NumShips(2)) {
 	    if (pw.Production(1) > pw.Production(2)) {
-		numFleets = 1;
-		attackMode = true;
+			numFleets = 1;
+			attackMode = true;
 	    } else {
-		numFleets = 3;
+			numFleets = 3;
 	    }
 	} else {
 	    if (pw.Production(1) > pw.Production(2)) {
-		numFleets = 1;
+			numFleets = 1;
 	    } else {
-		numFleets = 5;
+			numFleets = 5;
 	    }	    
 	}
 	// (1) If we current have more tha numFleets fleets in flight, just do
@@ -24,63 +24,57 @@ public class DualBot {
 	    return;
 	}
 	// (2) Find my strongest planet.
-	Planet source = null;
-	double sourceScore = Double.MIN_VALUE;
-	for (Planet p : pw.MyPlanets()) {
-	    double score = (double)p.NumShips() / (1 + p.GrowthRate());
+	int source = -1;
+	double sourceScore = -1;
+	for (size_t p = 0; p < pw.NumPlanets(); ++p) {
+		if(pw.state.planets[p].owner != 1) continue;
+	    double score = (double)pw.state.planets[p].numShips / (1 + pw.desc.planets[p].growthRate);
 	    if (score > sourceScore) {
-		sourceScore = score;
-		source = p;
+			sourceScore = score;
+			source = p;
 	    }
 	}
 	// (3) Find the weakest enemy or neutral planet.
-	Planet dest = null;
-	double destScore = Double.MIN_VALUE;
-	List<Planet> candidates = pw.NotMyPlanets();
+	int dest = -1;
+	double destScore = -1;
+	std::vector<Planet> candidates = pw.NotMyPlanets();
 	if (attackMode) {
 	    candidates = pw.EnemyPlanets();
 	}
-	for (Planet p : candidates) {
-	    double score = (double)(1 + p.GrowthRate()) / p.NumShips();
+	for (std::vector<Planet>::iterator p = candidates.begin(); p != candidates.end(); ++p) {
+	    double score = (double)(1 + p->growthRate) / p->numShips;
 	    if (score > destScore) {
-		destScore = score;
-		dest = p;
+			destScore = score;
+			dest = p->planetId;
 	    }
 	}
 	// (4) Send half the ships from my strongest planet to the weakest
 	// planet that I do not own.
-	if (source != null && dest != null) {
-	    int numShips = source.NumShips() / 2;
+	if (source >= 0 && dest >= 0) {
+	    int numShips = pw.state.planets[source].numShips / 2;
 	    pw.IssueOrder(source, dest, numShips);
 	}
-    }
-
-    public static void main(String[] args) {
-	String line = "";
-	String message = "";
-	int c;
-	try {
-	    while ((c = System.in.read()) >= 0) {
-		switch (c) {
-		case '\n':
-		    if (line.equals("go")) {
-			PlanetWars pw = new PlanetWars(message);
-			DoTurn(pw);
-		        pw.FinishTurn();
-			message = "";
-		    } else {
-			message += line + "\n";
-		    }
-		    line = "";
-		    break;
-		default:
-		    line += (char)c;
-		    break;
-		}
-	    }
-	} catch (Exception e) {
-	    // Owned.
-	}
-    }
 }
 
+
+int main(int argc, char *argv[]) {
+	std::string current_line;
+	std::string map_data;
+	while (true) {
+		int c = std::cin.get();
+		current_line += (char)c;
+		if (c == '\n') {
+			if (current_line.length() >= 2 && current_line.substr(0, 2) == "go") {
+				Game game;
+				game.ParseGameState(map_data);
+				map_data = "";
+				DoTurn(game);
+				game.FinishTurn();
+			} else {
+				map_data += current_line;
+			}
+			current_line = "";
+		}
+	}
+	return 0;
+}
